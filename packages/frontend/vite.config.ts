6edcf97fcdc0dc83d8e73f71eb000c4b7043627d
Eddie <edward@dcspark.io>
2025-07-29 00:29:57 -0400

    Added Midnight Startup, bumped version 0.3.15. Added Avail files.

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import deno from "@deno/vite-plugin";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
-
+import wasm from "vite-plugin-wasm";
 import "react";
 import "react-dom";
 
@@ -16,6 +16,7 @@ export default defineConfig({
     react(),
     deno(),
     nodePolyfills(),
+    wasm(),
   ],
   optimizeDeps: {
     include: ["react/jsx-runtime"],
